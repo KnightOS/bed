083965b4accd6457c99ade7f4211b85856538259
Drew DeVault <sir@cmpwn.com>
2015-10-20 08:18:39 -0400

    Pre-fill file name when saving files

--- src/actions.asm
+++ src/actions.asm
@@ -1,11 +1,20 @@
 action_exit:
     pcall(exitThread)
 action_save:
-    ; TODO: Prepopulate this with existing file name
     kld(hl, save_prompt)
     ld bc, 0x100
     pcall(malloc)
     ld (ix), 0
+    push hl
+        kld(hl, (file_name))
+        ld bc, 0
+        pcall(cpHLBC)
+        jr z, _
+        pcall(strlen)
+        inc bc
+        push ix \ pop de
+        ldir
+_:  pop hl
     corelib(promptString)
     or a
     kjp(z, draw_loop)
